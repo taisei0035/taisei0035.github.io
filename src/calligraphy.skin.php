4196c92b12fbe000afce0a1c2c1bcba0d3ea1d45
taisei0035 <38974778+taisei0035@users.noreply.github.com>
2018-05-04 17:56:47 +0900

    Create calligraphy.skin.php

---
+++ src/calligraphy.skin.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css">
+	<link rel="stylesheet" type="text/css" href="calligraphy.css">
+	<title>calligraphy</title>
+</head>
+<body>
+	<header id="header">
+		<div class="header_inner">
+			<h1 class="logo"><a href="/"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRE1e5zDP0uE1ii08UOO55m59P22QBspaFveNQrXjHGLR1p6fEQ" alt="calligraphy"></a></h1>
+			<ul class="header_nav">
+				<li><a href="">Calligraphyについて</a></li>
+				<li><a href="">オーダーメイド</a></li>
+				<li><a href="">書家プロフィール</a></li>
+				<li><a href="">問い合わせ</a></li>
+				<li>
+					<a href="" class="img">
+						<img src="https://image.freepik.com/free-icon/no-translate-detected_318-50865.jpg">
+					</a>
+					<a href="" class="img">
+						<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRw5VLdzIi7N8cz6Eg5AH65WTvc8Iisjj5nXSQX6KbQR__A8hO7Qg">
+					</a>
+				</li>
+			</ul>
+			<p class="language">
+				<a href="">▶︎<span>English</span></a>
+			</p>
+		</div>
+	</header>
+
+	<nav id="navigation">
+			<ul>
+				<a href="">
+					<li>おすすめ</li>
+				</a>
+				<a href="">
+					<li>額縁</li>
+				</a>
+				<a href="">
+					<li>掛け軸</li>
+				</a>
+				<a href="">
+					<li>うちわ</li>
+				</a>
+				<a href="">
+					<li>アクリル</li>
+				</a>
+				<a href="">
+					<li>スマホケース</li>
+				</a>
+			</ul>
+	</nav>
+
+	<nav class="bread-crumbs">
+		<ul>
+            <li itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb">
+                <a href="">
+        			<span>calligraphy</span>
+      			</a>
+      			<i> ＞ </i>
+            </li>
+            <li>
+        		<span class="now-bread">おすすめ</span>
+            </li>
+        </ul>
+	</nav>
+
+	<section id="main">
+		<div class="main-wrap">
+			<div class="main-left">
+				<h2 class="left-title">絞り込み検索</h2>
+				<div class="left-box">
+					<div class="left-white">
+						<div class="left-wrap">
+							<p class="desc">価格</p>
+							<select class="price" name="price">
+								<option label="下限なし" value="下限なし"></option>	
+								<option label="1,000円" value="1,000円"></option>
+								<option label="2,000円" value="2,000円"></option>
+							</select>
+							<label for="price"></label>	
+							<span>~</span>
+							<select class="price" name="price">	
+								<option label="上限なし" value="上限なし"></option>
+								<option label="1,000円" value="1,000円"></option>
+								<option label="2,000円" value="2,000円"></option>
+							</select>
+							<label for="price"></label>	
+						</div>
+						<div class="left-wrap">
+							<p class="desc">サイズ</p>
+							<select class="size" name="size">	
+								<option label="下限なし" value="下限なし"></option>	
+								<option label="1,000円" value="1,000円"></option>
+								<option label="2,000円" value="2,000円"></option>
+							</select>
+							<label for="size"></label>	
+							<span>~</span>
+							<select class="size" name="size">	
+								<option label="上限なし" value="上限なし"></option>	
+								<option label="1,000円" value="1,000円"></option>
+								<option label="2,000円" value="2,000円"></option>
+							</select>
+							<label for="size"></label>	
+						</div>
+						<div class="left-wrap">
+							<p class="desc">タイプ</p>
+							<select class="size" name="size">	
+								<option label="下限なし" value="下限なし"></option>	
+								<option label="1,000円" value="1,000円"></option>
+								<option label="2,000円" value="2,000円"></option>
+							</select>
+							<label for="size"></label>	
+							<span>~</span>
+							<select class="size" name="size">	
+								<option label="上限なし" value="上限なし"></option>	
+								<option label="1,000円" value="1,000円"></option>
+								<option label="2,000円" value="2,000円"></option>
+							</select>
+							<label for="size"></label>	
+						</div>
+						<div class="submit">
+					        <input type="hidden" name="ofsubmitted" value="1">
+							<input type="submit" value="この条件で検索する">
+				        </div>
+					</div>
+				</div>
+			</div>
+
+			<div class="main-right">
+				<div class="right-wrap">
+					<h2 class="right-top">おすすめ作品</h2>
+					<div class="right-content">
+						<div class="hed-img">
+							<a href="">
+								<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToGUwXiMFd3-TpbostK4jQLIeDy2OJaFdBx-SH4Lqx2RuGDENz">
+							</a>
+						</div>
+						<div class="right-bottom">
+							<a href="">
+								<div class="bot-header">
+									<p class="bot-name">「糖分」</p>
+									<p class="bot-size">15 × 30cm</p>
+								</div>
+							</a>
+							<div class="bot-bottom">
+								<p class="bot-price">￥20,000<span>(税込)</span></p>
+								<p class="bot-like"><span>🖤3</span></p>
+							</div>
+						</div>
+					</div>
+
+					<div class="right-content">
+						<div class="hed-img">
+							<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToGUwXiMFd3-TpbostK4jQLIeDy2OJaFdBx-SH4Lqx2RuGDENz">
+						</div>
+						<div class="right-bottom">
+							<div class="bot-header">
+								<p class="bot-name">「糖分」</p>
+								<p class="bot-size">15 × 30cm</p>
+							</div>
+							<div class="bot-bottom">
+								<p class="bot-price">￥20,000<span>(税込)</span></p>
+								<p class="bot-like">
+									<span>🖤3</span>
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="right-content">
+						<div class="hed-img">
+							<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToGUwXiMFd3-TpbostK4jQLIeDy2OJaFdBx-SH4Lqx2RuGDENz">
+						</div>
+						<div class="right-bottom">
+							<div class="bot-header">
+								<p class="bot-name">「糖分」</p>
+								<p class="bot-size">15 × 30cm</p>
+							</div>
+							<div class="bot-bottom">
+								<p class="bot-price">￥20,000<span>(税込)</span></p>
+								<p class="bot-like"><span>🖤3</span></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div id="paginate">
+                <p>1</p>
+                <p>2</p>
+                <p>3</p>
+            </div>
+		</div>
+	</section>
+
+	<div id="footer">
+		<div class="footer-wrap">
+			<div class="footer-left">
+				<div class="ft-left-wrap">
+					<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRE1e5zDP0uE1ii08UOO55m59P22QBspaFveNQrXjHGLR1p6fEQ" alt="calligraphy">
+				</div>
+			</div>
+			<div class="footer-right">
+				<ul>
+					<li>
+						<a href="">calligraphyについて</a>
+					</li>
+					<li>
+						<a href="">ご利用規約</a>
+					</li>
+					<li>
+						<a href="">個人情報保護方針</a>
+					</li>
+					<li>
+						<a href="">特定商取引について</a>
+					</li>
+					<li>
+						<a href="">コンプライアンスポリシー</a>
+					</li>
+					<li>
+						<a href="">資金決済に基づく表示</a>
+					</li>
+					<li>
+						<a href="">返品・返金について</a>
+					</li>
+					<li>
+						<a href="">ヘルプ</a>
+					</li>
+					<li>
+						<a href="">法人の方</a>
+					</li>
+					<li>
+						<a href="">書道家の方</a>
+					</li>
+					<li>
+						<a href="">掲載希望の方</a>
+					</li>
+					<li>
+						<a href="">オーダーメイドをご希望の方</a>
+					</li>
+					<li>
+						<a href="">お問い合わせ</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
